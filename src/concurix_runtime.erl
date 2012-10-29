6cb0b60e4296b21a4422dcdd668f5f101738ae8b
alexgo <alex@gounares.com>
2012-10-29 11:36:00 -0700

    added support for master config

--- src/concurix_runtime.erl
+++ src/concurix_runtime.erl
@@ -12,25 +12,22 @@ start(Filename) ->
 	io:format("starting Concurix Runtime~n"),
 	Dirs = code:get_path(),
 	{ok, Config, _File} = file:path_consult(Dirs, Filename),
-	setup_ets_tables(),
+	setup_ets_tables([concurix_config_master, concurix_config_spawn, concurix_config_memo]),
 	setup_config(Config).
 	
 %% we setup ets tables for configuration now to simplify the compile logic.  this
 %% way every table is available even if the particular customer instance does not
 %% have any options
 
-setup_ets_tables() ->
-	case ets:info(concurix_config_spawn) of
-		undefined -> ok;
-		_X -> ets:delete(concurix_config_spawn)
+setup_ets_tables([]) ->
+	ok;
+setup_ets_tables([H | T]) ->
+	case ets:info(H) of
+		undefined ->ok;
+		_X -> ets:delete(H)
 	end,
-	ets:new(concurix_config_spawn, [named_table, {read_concurrency, true}, {heir, whereis(init), concurix}]),
-		
-	case ets:info(concurix_config_memo) of
-		undefined -> ok;
-		_Y -> ets:delete(concurix_config_memo)
-	end,
-	ets:new(concurix_config_memo, [named_table, {read_concurrency, true}, {heir, whereis(init), concurix}]).
+	ets:new(H, [named_table, {read_concurrency, true}, {heir, whereis(init), concurix}]),
+	setup_ets_tables(T).
 	
 setup_config([]) ->
 	ok;
@@ -39,6 +36,9 @@ setup_config([{spawn, SpawnConfig} | Tail]) ->
 	setup_config(Tail);
 setup_config([{memoization, MemoConfig} | Tail]) ->
 	lists:foreach(fun(X) -> {{M, F}, Expr} = X, ets:insert(concurix_config_memo, {{M, F}, Expr}) end, MemoConfig),
+	setup_config(Tail);
+setup_config([{master, MasterConfig} | Tail]) ->
+	lists:foreach(fun(X) -> {Key, Val} = X, ets:insert(concurix_config_master, {Key, Val}) end, MasterConfig),
 	setup_config(Tail);
 setup_config([Head | Tail]) ->
 	%% do something with head
