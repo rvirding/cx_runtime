6bf6eedf121718a7ff216ef618885d76b7050aec
alexgo <alex@gounares.com>
2013-03-19 22:00:36 -0700

    cleanup

--- src/concurix_web_socket.erl
+++ src/concurix_web_socket.erl
@@ -1,6 +1,5 @@
 -module(concurix_web_socket).
 -compile(export_all).
--author({jha, abhinav}).
 
 
 start() ->
@@ -28,68 +27,27 @@ handle(S)->
 
 handle(S, _Data)->
     Pid = self(),
-    Child = spawn(fun() -> handshake_and_talk(Pid) end),
     gproc:reg({p, l, "benchrun_tracing"}),
-    loop(S, Child).
+    loop(S).
 
-loop(S, Child)->
+loop(S)->
     receive
         {tcp, S, Bin} -> 
-            Child ! {self(), Bin}, 
-            loop(S, Child);
-        {Child, X} ->
-            io:format("sending ~p to ~p ~n", [X, S]),
-            gen_tcp:send(S, X),
-            loop(S, Child);
-        {trace, Json} -> 
-            io:format("sending json ~p ~n", [lists:flatten(Json)]),
-            websocket_send_text(S, Json),
-            loop(S, Child);
-        _Any ->
-            loop(S, Child)
-    end.
-            
-
-% Handshake with the client and begin talking.
-handshake_and_talk(Ppid)->
-    receive
-        {Ppid, X} ->
-            io:format("got connection request of ~p ~n", [X]),
-            % Body = the checksum comprised after processing Key1, Key2 and the Client's request body.
-            %Body = process_client_handshake(binary_to_list(X)),
-            Challenge = process_handshake_key(X),
+            Challenge = process_handshake_key(Bin),
             % Send the Handshake stuff to the browser.
-            Ppid ! {self(), "HTTP/1.1 101 Switching Protocols\r\n"},
-            Ppid ! {self(), "Upgrade: websocket\r\n"},
-            Ppid ! {self(), "Connection: Upgrade\r\n"},
+            gen_tcp:send(S, "HTTP/1.1 101 Switching Protocols\r\n"),
+            gen_tcp:send(S, "Upgrade: websocket\r\n"),
+            gen_tcp:send(S, "Connection: Upgrade\r\n"),
             %%Ppid ! {self(), "Sec-WebSocket-Origin: http://127.0.0.1:8080\r\n"},
             %%Ppid ! {self(), "Sec-WebSocket-Location: ws://127.0.0.1:8000/\r\n"},
-            %%Ppid ! {self(), "Sec-WebSocket-Protocol: chat\r\n"},
-            Ppid ! {self(), "Sec-WebSocket-Accept: " ++ Challenge ++ "\r\n" },
-            Ppid ! {self(), "\r\n"};
-
-            % Send the body. 
-            %Ppid ! {self(), Body};
-
-            % Now call the talk method to do the actual talking with the browser.
-            % talk(Ppid);
-
-        Any -> io:format("[Child] Random stuff received:~p~n. ~p", [Any, Ppid]) 
-    end.
-
-
-% Function to actuall talk to the browser.
-% Dummy implementation -  Implement as reuired.
-talk(Browser) ->
-    receive
-    after 1000 ->
-            % This is the main communicator function to the Browser.
-            % Whatever you write instead of "Hahah" will get sent to the browser.
-            % the 0 and 255 is the framing ( required) - don't change that. 
-            Browser ! {self(), [0]},
-            Browser ! {self(), "Hahah"},
-            Browser ! {self(), [255]},
-            talk(Browser)
+            gen_tcp:send(S, "Sec-WebSocket-Accept: " ++ Challenge ++ "\r\n"),
+            gen_tcp:send(S, "\r\n"),
+            loop(S);
+        {trace, Json} -> 
+            websocket_send_text(S, Json),
+            loop(S);
+        _Any ->
+            loop(S)
     end.
 
 process_handshake_key(X) ->
@@ -109,41 +67,6 @@ token_to_kv(Token) ->
     Y ->
       {yikes, Y}
   end.
-  
-% Process client's handshake to retrieve information.
-process_client_handshake(X)->
-    [Body|Head] = lists:reverse(string:tokens(X, "\r\n")),
-    {Key1, Key2} = extract_keys(lists:reverse(Head)),
-    {Skey1, Skey2} = process_keys(Key1, Key2),
-    Bin_body = list_to_binary(Body),
-    Key = <<Skey1:32/big-unsigned-integer, Skey2:32/big-unsigned-integer, Bin_body/binary>>,
-    erlang:md5(Key).
-
-% Extract keys from the client's handshake.
-extract_keys([H|T])->
-    Key1 = extract_key("Sec-WebSocket-Key: ", [H|T]),
-    Key2 = extract_key("Sec-WebSocket-Key2: ", [H|T]),
-    {Key1, Key2}.
-
-extract_key(X, [H|T])->
-    case string:str(H, X) of
-        0 -> extract_key(X, T); 
-        _Pos -> string:substr(H, string:len(X) + 1)
-    end.
-
-% Process the keys as mentioned in the handshake 76 draft of the ietf.
-process_keys(Key1, Key2)->
-    {Digits1, []} = string:to_integer(digits(Key1)),
-    {Digits2, []} = string:to_integer(digits(Key2)),
-    Spaces1 = spaces(Key1),
-    Spaces2 = spaces(Key2),
-    {Digits1 div Spaces1, Digits2 div Spaces2}.
-
-% Concatenate digits 0-9 of a string 
-digits(X)-> [A || A<-X, A =< 57, A >= 48].
-
-% Count number of spaces in a string.
-spaces(X)-> string:len([ A || A<-X, A =:= 32]).
 
 websocket_opcode(text) -> 1;
 websocket_opcode(binary) -> 2;
