62eb26065110f0042ddee4d3108848545769872d
Michael Noakes <noakes@concurix.com>
2013-03-23 11:47:15 -0700

    Pull in a few functions from erlcloud to support writing files to S3

--- src/concurix_trace_send_to_S3.erl
+++ src/concurix_trace_send_to_S3.erl
@@ -51,11 +51,11 @@ handle_info(send_snapshot, State) ->
       ok
   end,
 
-  RunInfo             = State#tcstate.runInfo,
-  Url                 = proplists:get_value(trace_url, RunInfo),
-  Fields              = snapshot_fields(RunInfo),
-  Json                = concurix_runtime:get_current_json(State),
-  Request             = erlcloud_s3:make_post_http_request(Url, Fields, Json),
+  RunInfo = State#tcstate.runInfo,
+  Url     = proplists:get_value(trace_url, RunInfo),
+  Fields  = snapshot_fields(RunInfo),
+  Json    = concurix_runtime:get_current_json(State),
+  Request = s3_make_post_http_request(Url, Fields, Json),
 
   httpc:request(post, Request, [{timeout, 60000}], [{sync, true}]),
 
@@ -92,4 +92,61 @@ terminate(_Reason, _State) ->
  
 code_change(_oldVsn, State, _Extra) ->
   {ok, State}.
+
+
+
+
+
+
+
+
+
+
+
+
+%%
+%% This function is taken from erlcloud_s3.erl
+%%
+
+s3_make_post_http_request(Url, Fields, Data) ->
+  Boundary = "-------erlcloud_s3---AG121712-----",
+  Body     = format_multipart_formdata(Boundary, Fields, Data),
+
+  {Url, [{"Content-Length"}, integer_to_list(length(Body))], "multipart/form-data; boundary=" ++ Boundary, Body}.
+
+%%
+%% credit to http://lethain.com/formatting-multipart-formdata-in-erlang/ for the original code here
+%%
+format_multipart_formdata(Boundary, Fields, Data) when is_binary(Data) ->
+  format_multipart_formdata(Boundary, Fields, binary_to_list(Data), "application/octet-stream");
+
+format_multipart_formdata(Boundary, Fields, Data) when is_list(Data) ->
+  format_multipart_formdata(Boundary, Fields, Data, "text/plain").
+
+format_multipart_formdata(Boundary, Fields, Data, Type) ->
+  FieldParts  = lists:map(fun({FieldName, FieldContent}) ->
+                            [lists:concat(["--", Boundary]),
+                             lists:concat(["Content-Disposition: form-data; name=\"",
+                                           atom_to_list(FieldName),
+                                           "\""]),
+                             "",
+                             FieldContent]
+                            end, 
+                          Fields),
+  FieldParts2 = lists:append(FieldParts),
+
+  FileParts   = [[lists:concat(["--", Boundary]),
+                  lists:concat(["Content-Disposition: form-data; name=\"file\""]),
+                  lists:concat(["Content-Type: ", Type]),
+                  "",
+                  Data]],
+  FileParts2  = lists:append(FileParts),
+
+  EndingParts = [lists:concat(["--", Boundary, "--"]), ""],
+
+  Parts       = lists:append([FieldParts2, FileParts2, EndingParts]),
+
+  string:join(Parts, "\r\n").
+
+
 
