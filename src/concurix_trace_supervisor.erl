bae8ff529317a020846df8809e4f227736fd6755
alexgo <alex@gounares.com>
2013-07-09 15:33:38 -0700

    removed warning on unused feature

--- src/concurix_trace_supervisor.erl
+++ src/concurix_trace_supervisor.erl
@@ -32,13 +32,11 @@ init([State, Options]) ->
   Terminate  = 2 * 1000,
 
   StateTrace = State#tcstate{collectTraceData = enable_trace(Options)},
-  StateProf  = State#tcstate{collectTraceData = enable_prof (Options)},
   StateViz   = State#tcstate{sendUpdates      = enable_viz  (Options)},
   StateS3    = State#tcstate{sendUpdates      = enable_s3   (Options)},
 
   Children   = [
                  {proc, {concurix_trace_by_process,   start_link, [StateTrace]}, transient, Terminate, worker, [concurix_trace_by_process]},
-                 {prof, {concurix_trace_by_scheduler, start_link, [StateProf]},  transient, Terminate, worker, [concurix_trace_by_scheduler]},
                  {viz,  {concurix_trace_send_to_viz,  start_link, [StateViz]},   transient, Terminate, worker, [concurix_trace_send_to_viz]},
                  {s3,   {concurix_trace_send_to_S3,   start_link, [StateS3]},    transient, Terminate, worker, [concurix_trace_send_to_S3]}
                ],
@@ -47,9 +45,6 @@ init([State, Options]) ->
 
 
 enable_trace(_Options) ->
-  true.
-
-enable_prof(_Options) ->
   true.
 
 enable_viz(_Options) ->
